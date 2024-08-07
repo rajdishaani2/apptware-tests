6d21345f88c723406e9dc9b2558a92125b171e52
rajdishaani2 <87628716+rajdishaani2@users.noreply.github.com>
2024-08-07 13:47:27 +0530

    Update InterviewApplication.java

--- src/main/java/com/apptware/interview/InterviewApplication.java
+++ src/main/java/com/apptware/interview/InterviewApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 class InterviewApplication {
-  //
   public static void main(String[] args) {
     SpringApplication.run(InterviewApplication.class, args);
   }
